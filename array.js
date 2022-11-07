e35577f0cac44dd0a9bd0f3cd78276f1c6331145
unknown <md.shajib001@gmail.com>
2022-11-07 18:45:20 +0600

    new line added

--- array.js
+++ array.js
@@ -1,7 +1,19 @@
-var myArray=[1,2,3,4,5,6,7];
-var myArrayPro= New Array(,1,2,3,4,5,6,7);
+// //  var myArray=[1,2];
+// // var myArrayPro=New Array:(1,2,3,4,5,6,7);
 
-var let item of myArray(){
-    console.log(item);
 
-}
+// // for(let item of myArrayPro){
+// //     console.log(item);
+
+// // }
+// var Bangladesh=["Dhaka", "Comilla", "Chittagong"]
+// var India=["Kolkata", "Mumbai", "Tamil"]
+// var America=["NewYork", "texas", "island"]
+
+// var Asia=[Bangladesh,India,America];
+// console.log(Asia[2][0]);
+
+var countries=["bangladesh",'india','Pakistan','Honulolo']
+
+var [a,,b,c]=countries
+console.log(c);
---
+++ es6set.js
@@ -0,0 +1,11 @@
+var myset= new Set();
+
+myset.add('Bangladesh');
+myset.add('India');
+myset.add('Nepal');
+myset.add('Butan');
+myset.add('Srilanka');
+myset.add('Pakistan');
+
+
+console.log(myset);
